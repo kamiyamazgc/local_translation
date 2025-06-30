a4c374958d791b47023715c4e3a49bbb15d0a761
kamiyamazgc <148608692+kamiyamazgc@users.noreply.github.com>
2025-07-01 03:03:36 +0900

    Add environment variable for server URL

--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -15,8 +15,8 @@ from typing import Optional
 
 class Summarizer:
     """LM Studioを使用したローカル要約クラス"""
-    
-    def __init__(self, server_url: str = "http://127.0.0.1:1234"):
+
+    def __init__(self, server_url: str = os.getenv("LM_STUDIO_SERVER_URL", "http://127.0.0.1:1234")):
         """
         初期化
         
